de74d25c79acc61c26e8dcd8ead4117082ce6a68
Andrey Galeazzi <andreymofo@gmail.com>
2025-07-17 23:51:11 -0300

    Use theme values for css

--- src/components/ui/GrantStatusTracker.tsx
+++ src/components/ui/GrantStatusTracker.tsx
@@ -7,9 +7,9 @@ export default function GrantStatusTracker() {
         <Grid container spacing={2} my={2} mx={2} sx={{ backgroundColor: '#f4f5f7', width: '100%', height: 'auto' }}>
             <Grid size={12}>
                 <Typography variant="h4" sx={(theme) => ({
-                    color: theme.palette.primary.main,
-                    fontWeight: 700,
-                    fontFamily: 'sans-serif',
+                    color: theme.palette.text.primary,
+                    fontWeight: theme.typography.fontWeightBold,
+                    fontFamily: theme.typography.fontFamily,
                 })}>
                     Grant Status Tracker
                 </Typography>
